1b0a299bd49557a8e270faa8c70ee2d26242a9a0
mbayajames <jmuthiora244@gmail.com>
2026-02-03 03:34:29 -0800

    Enhance NavLink component implementation

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,25 +1,25 @@
+import { NavLink as RouterNavLink } from "react-router-dom";
+import type { NavLinkProps } from "react-router-dom";
 import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
 
-interface NavLinkCompatProps {
+interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
   className?: string;
   activeClassName?: string;
   pendingClassName?: string;
-  to: string;
-  children?: React.ReactNode;
 }
 
 const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
-  ({ className, to, children, ...props }, ref) => {
+  ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
     return (
-      <a
+      <RouterNavLink
         ref={ref}
-        href={to}
-        className={cn(className)}
+        to={to}
+        className={({ isActive, isPending }) =>
+          cn(className, isActive && activeClassName, isPending && pendingClassName)
+        }
         {...props}
-      >
-        {children}
-      </a>
+      />
     );
   },
 );
